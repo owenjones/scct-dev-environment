eacb09e98cef889686f039c260f1a2e139ab39a2
Owen Jones <owen@owenjones.net>
2024-06-20 15:03:31 +0100

    phpmyadmin configuration table and operartions user setup

--- .devcontainer/mariadb/init/0-init.sql
+++ .devcontainer/mariadb/init/0-init.sql
@@ -1,3 +1,8 @@
+-- phpmyadmin configuration user and database
+CREATE USER 'phpmyadmin'@'%' IDENTIFIED BY '60cfd180192e88712c3d1d638bba67730ce5';
+GRANT SELECT, INSERT, UPDATE, DELETE ON `phpmyadmin`.* TO 'phpmyadmin'@'%';
+
+-- setup scct user privileges
 GRANT ALL PRIVILEGES ON `scct\_%`.* TO 'scct'@'%' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON `sakila`.* TO 'scct'@'%';
 GRANT ALL PRIVILEGES ON `studentsreg`.* TO 'scct'@'%';
